70d55eaf5dd98b3cf4ef2056cb1982ad8f26a93f
Teo56 <mp1319@ic.ac.uk>
2021-12-02 12:41:10 +0000

    Initial commit

---
+++ src/main/java/com/example/auto_sign/AutoSignApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto_sign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoSignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auto_sign/AutoSignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto_sign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

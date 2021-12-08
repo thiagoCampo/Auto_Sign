1ffdc88bd9cbeddbcff7afcefc427ff1bf8a7e45
Teo56 <mp1319@ic.ac.uk>
2021-12-08 10:37:57 +0000

    form with input url

--- src/main/java/com/example/auto_sign/HomeController.java
+++ src/main/java/com/example/auto_sign/HomeController.java
@@ -1,6 +1,10 @@
 package com.example.auto_sign;
 
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,8 +12,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("home", new HomeView());
         return "home";
     }
 
+    @PostMapping("/")
+    public String URLsubmit(@ModelAttribute HomeView home, Model model) {
+        model.addAttribute("home", home);
+        System.out.println(home.getURL());
+        return "result";
+    }
+
 }
+
+
+
+
---
+++ src/main/java/com/example/auto_sign/HomeView.java
@@ -0,0 +1,14 @@
+package com.example.auto_sign;
+
+public class HomeView {
+
+    private String url;
+
+    public String getURL() {
+        return url;
+    }
+
+    public void setURL(String url) {
+        this.url = url;
+    }
+}
